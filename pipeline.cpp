2e71c64cba6ffcdac72a8016eaea90275ad61214
LopsidedMango <tomomatson@icloud.com>
2026-02-16 23:17:05 +0000

    Fixed pipeline

--- pipeline.cpp
+++ pipeline.cpp
@@ -143,7 +143,7 @@ void Pipeline::createShaderModule(const std::vector<char>& code, VkShaderModule*
 
 
 PipelineConfigInfo Pipeline::defaultPipelineConfigInfo(uint32_t width, uint32_t height){
-  PipelineConfigInfo configInfo;
+  PipelineConfigInfo configInfo{};
   
   configInfo.inputAssemblyInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
   configInfo.inputAssemblyInfo.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
