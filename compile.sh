df39826a1646f783ba7c942513b2a9c749b37763
LopsidedMango <tomomatson@icloud.com>
2026-02-16 17:26:24 +0000

    Added vertex and fragment shader

---
+++ compile.sh
@@ -0,0 +1,3 @@
+/Users/school/VulkanSDK/1.4.341.0/macOS/bin/glslc shaders/simple_shader.vert -o  shaders/simple_shader.vert.spv 
+/Users/school/VulkanSDK/1.4.341.0/macOS/bin/glslc shaders/simple_shader.frag -o  shaders/simple_shader.frag.spv 
+
