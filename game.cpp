39f26668a557c0ad40b26a7a1f4ec8abb4e1f3c3
LopsidedMango <tomomatson@icloud.com>
2026-02-26 00:20:01 +0000

    Updated camera to allow for view transformations

--- game.cpp
+++ game.cpp
@@ -30,6 +30,9 @@ void Game::run() {
   RenderSystem renderSystem{device, renderer.getSwapChainRenderPass()};
 
   Camera camera{};
+  // camera.setViewDirection(glm::vec3(0.0f), glm::vec3(0.5f, 0.0f, 1.0f));
+  camera.setViewTarget(glm::vec3(-1.0f, -2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 2.5f));
+
 
 
   while(!window.shouldClose()){
--- render_system.cpp
+++ render_system.cpp
@@ -75,6 +75,8 @@ void RenderSystem::createPipeline(VkRenderPass renderPass){
 void RenderSystem::renderGameObjects(VkCommandBuffer commandBuffer, std::vector<GameObject>& gameObjects, const Camera& camera){
   pipeline->bind(commandBuffer);
 
+  auto projectionView = camera.getProjection() * camera.getView();
+
   for (auto& obj : gameObjects){
 
     obj.transform.rotation.y = glm::mod(obj.transform.rotation.y + 0.01f, glm::two_pi<float>());
@@ -83,7 +85,7 @@ void RenderSystem::renderGameObjects(VkCommandBuffer commandBuffer, std::vector<
 
     SimplePushConstantData push{};
     push.color = obj.color;
-    push.transform = camera.getProjection() * obj.transform.mat4();
+    push.transform = projectionView * obj.transform.mat4();
 
     vkCmdPushConstants(commandBuffer, pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0, sizeof(SimplePushConstantData), &push);
 
