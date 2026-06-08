222d1f8878ffea030a2e905154d4c44c7d23334c
LopsidedMango <tomomatson@icloud.com>
2026-06-08 16:26:10 +0100

    Update allocator concepts

--- engine/containers/allocator_concepts.hpp
+++ engine/containers/allocator_concepts.hpp
@@ -10,7 +10,6 @@ template<typename A>
 concept Allocator = 
 requires (A a, std::size_t size, std::size_t alignment, void* ptr, std::size_t bytes) {
   { a.allocate(size, alignment) } -> std::same_as<void*>;
-  { a.deallocate(ptr, bytes) } -> std::same_as<void>;
 };
 
 } // namespace Engine::Containers
