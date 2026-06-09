49e50aaff7872f147fd412e2d6e42d9f17c7bfc5
LopsidedMango <tomomatson@icloud.com>
2026-06-09 12:54:17 +0100

    Update memory_overrides for thread safety

--- engine/memory/memory_overrides.cpp
+++ engine/memory/memory_overrides.cpp
@@ -3,52 +3,53 @@
 #include <new>
 #include <cstdlib>
 #include <cstdio>
+#include <atomic>
 
 namespace {
-  bool external_scope = false;
+  thread_local int external_scope_depth = 0;
 }
 
 ExternalAllocationScope::ExternalAllocationScope()
 {
-  external_scope = true; 
+  external_scope_depth++; 
 }
 
 ExternalAllocationScope::~ExternalAllocationScope()
 {
-  external_scope = false; 
+  external_scope_depth--; 
 }
 
 namespace {
 struct Stats {
-  std::size_t internal_allocations = 0; 
-  std::size_t external_allocations = 0; 
+  std::atomic<std::size_t> internal_allocations{0}; 
+  std::atomic<std::size_t> external_allocations{0}; 
 } stats;
 }
 
 void* operator new(std::size_t size) 
 {
-  if (external_scope) stats.external_allocations++;
+  if (external_scope_depth > 0) stats.external_allocations++;
   else stats.internal_allocations++;
   return std::malloc(size);
 }
 
 void* operator new[](std::size_t size)
 {
-  if (external_scope) stats.external_allocations++;
+  if (external_scope_depth > 0) stats.external_allocations++;
   else stats.internal_allocations++;
   return std::malloc(size);
 }
 
 void operator delete(void* ptr) noexcept
 {
-  if (external_scope) stats.external_allocations++;
+  if (external_scope_depth > 0) stats.external_allocations++;
   else stats.internal_allocations++;
   std::free(ptr);
 }
 
 void operator delete[](void* ptr) noexcept  
 {
-  if (external_scope) stats.external_allocations++;
+  if (external_scope_depth > 0) stats.external_allocations++;
   else stats.internal_allocations++;
   std::free(ptr);
 }
@@ -56,9 +57,10 @@ void operator delete[](void* ptr) noexcept
 void MemoryTracker::report()
 {
   std::printf("=== Allocation Report ===\n");
-  std::printf("External : %zu\n", stats.external_allocations);
-  std::printf("Internal : %zu\n", stats.internal_allocations);
-  if (stats.internal_allocations > 0)
-      std::printf("WARNING: %zu internal allocation(s) detected!\n", stats.internal_allocations);
+  std::printf("External : %zu\n", stats.external_allocations.load());
+  std::printf("Internal : %zu\n", stats.internal_allocations.load());
+  if (stats.internal_allocations.load() > 0)
+      std::printf("WARNING: %zu internal allocation(s) detected!\n", stats.internal_allocations.load());
   std::printf("=========================\n");
 }
+
